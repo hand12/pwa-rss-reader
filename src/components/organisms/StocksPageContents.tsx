a1e8c7b2bd2f3f2fa37966a60a72bdb66d9f33d3
yusuke_yamashita <hand12.k@gmail.com>
2019-08-25 19:05:47 +0900

    topページへのリンクを設置

--- src/components/organisms/StocksPageContents.tsx
+++ src/components/organisms/StocksPageContents.tsx
@@ -1,4 +1,5 @@
 import React, { FC, useEffect } from 'react'
+import { Link } from 'react-router-dom'
 import Stocks from '../molecules/Stocks'
 import { Stock } from '../../ducks/stocks/types'
 import './StocksPageContents.scss'
@@ -21,7 +22,9 @@ const StocksPageContents: FC<StocksPageContentsProps> = ({ stocks, readStock, re
         <Stocks stocks={ stocks } readStock={ readStock } removeStock={ removeStock } />
       </div>
       <div className="linksContents">
-        
+        <Link to="/">
+          記事一覧へ戻る
+        </Link>
       </div>
     </div>
   )
