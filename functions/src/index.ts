2bf743787d1efe3a4bd756182b5c44edf53c83d2
Yusuke Yamashita <yusuke.yamashita@livesense.co.jp>
2019-09-02 13:02:13 +0900

    本番用の設定追加

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -4,8 +4,6 @@ admin.initializeApp(functions.config().firebase)
 
 import { fetchFeeds, saveFeeds, deleteFeeds } from './feeds'
 import { getFeeds } from './api/feeds'
-import PROJECTS from './utils/firebase/projects'
-
 
 export const updateFeeds = functions.pubsub.schedule('00 22 * * *').onRun(async (context) => {
   const feeds = await fetchFeeds()
@@ -23,12 +21,10 @@ export const updateFeeds = functions.pubsub.schedule('00 22 * * *').onRun(async 
 })
 
 export const apiFeedList = functions.https.onRequest(async (req, res) => {
-  const project = PROJECTS.find(p => p.id === process.env.GCLOUD_PROJECT)
-  if (!project) return res.status(200).json([]).end()
-
   const feeds = await getFeeds(req.query.genre)
 
-  const allowedOrigins = project.allowedOrigins
+  const allowedOrigins = functions.config().allowed_origins
+  console.log(allowedOrigins)
 
   const origin = req.headers.origin
 
