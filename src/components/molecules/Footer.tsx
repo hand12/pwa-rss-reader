d075fe0a86dd2505367017b11146fab67e8ecbc4
yusuke_yamashita <hand12.k@gmail.com>
2019-09-04 01:34:39 +0900

    リリース前の微調整

--- src/components/molecules/Footer.tsx
+++ src/components/molecules/Footer.tsx
@@ -10,11 +10,11 @@ const App: React.FC = () => (
         <div className="label">運営者</div>
         <div className="icon">
           <a
-            href="https://twitter.com/hand12_k"
+            href="https://twitter.com/news_collect_k"
             target="_blank"
             rel="noopener noreferrer">
               <FontAwesomeIcon icon={['fab', 'twitter']} />
-              hand12_k
+              @news_collect_k
           </a>
         </div>
       </div>
